198db7589432c10b5f9c319f084bf2acd99c2c34
Gui-hornig <guihornig@gmail.com>
2024-01-22 10:49:36 -0300

    JavaUpdate

---
+++ aula7/erros.java
@@ -0,0 +1,5 @@
+package Curso_Java.aula7;
+
+public class erros {
+    
+}
--- aula7Teste/Argumentos2.java
+++
@@ -1,7 +0,0 @@
-package Curso_Java.aula7Teste;
-
-public class Argumentos2 {
-    public static void main (String[] args) {
-        System.out.println("Voce disse:" + args[0]);
-    }
-}
