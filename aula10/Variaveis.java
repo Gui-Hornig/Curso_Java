3941bd6cfeb8bd9e0f14e89f7d992fa2f175bfbb
Gui-hornig <guihornig@gmail.com>
2024-01-25 10:42:48 -0300

    Java:Study

---
+++ aula10/Variaveis.java
@@ -0,0 +1,14 @@
+package Curso_Java.aula10;
+
+public class Variaveis {
+    public static void main (String[] args){
+
+        int idade = 20;
+        String nome = "Guilherme";
+        String NomeDoMeuCachorro = "Totó";
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+        System.out.println("NomeDoMeuCachorro = " + NomeDoMeuCachorro);
+    }
+}
